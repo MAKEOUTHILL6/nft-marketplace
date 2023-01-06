94e16ee00decfe4b141676b66973d3a147e675d9
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2023-01-06 21:47:59 +0200

    Added getter functions

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -88,4 +88,12 @@ contract NftMarketplace {
         (bool success, ) = payable(msg.sender).call{value: proceeds}("");
         require(success);
     }
+
+    function getListing(address nftAddress, uint256 tokenId) external view returns (Listing memory) {
+        return s_listings[nftAddress][tokenId];
+    }
+
+    function getProceeds(address seller) external view returns (uint256) {
+        return s_proceeds[seller];
+    }
 }
